cf57b4f564eb364de0fce17108b70203c847fc2d
GabrielPivoto <gabriel-pivoto@outlook.com>
2025-11-23 12:09:43 -0300

    Add DWA container

--- pyotest/config/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_DVWA = array();
-$_DVWA['recaptcha_public_key'] = '';
-$_DVWA['recaptcha_private_key'] = '';
-$_DVWA['default_security_level'] = 'low';
-$_DVWA['disable_authentication'] = false;
-?>
